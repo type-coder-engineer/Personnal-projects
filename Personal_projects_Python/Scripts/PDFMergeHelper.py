995b39259c27ab0cff9761cc001661d8a3e78056
ZHANG Chenyu <zcy.scott@gmail.com>
2017-05-01 17:31:06 +0200

    modify the script to merge the pdf

--- Personal_projects_Python/Scripts/PDFMergeHelper.py
+++ Personal_projects_Python/Scripts/PDFMergeHelper.py
@@ -1,6 +1,35 @@
 #coding: utf-8
 from PyPDF2 import PdfFileMerger
-import os, sys
+import os, sys, time
+
+def mergeTarget(targets, name):
+    path = os.getcwd()
+    allFile = os.listdir(path)
+    mergeFile = PdfFileMerger()
+    if '.pdf' not in name:
+        pdfOutput = name + '.pdf'
+    else:
+        pdfOutput = name
+    if pdfOutput in allFile:
+        targets.append(pdfOutput)
+        print 'Final target name "{}" already exists'.format(pdfOutput)
+        pdfOutput = 'merged-' + time.strftime('%Y%m%d-%H%M%S') + '.pdf'
+        print 'So the target is saved by the name "{}"'.format(pdfOutput)
+            
+    index = 0
+    for file in targets:
+        try:
+            target = open(file, 'rb')
+            mergeFile.append(target)
+            index += 1
+        except:
+            print 'Wrong with the file "{}"'.format(file)
+    target.close()
+    output = open(pdfOutput, 'wb')
+    mergeFile.write(output)
+    mergeFile.close()
+    print 'You have merged {} PDF files'.format(index)
+    print 'All done!'
 
 def mergeAll(name):
     path = os.getcwd()
@@ -10,10 +39,18 @@ def mergeAll(name):
         pdfOutput = name + '.pdf'
     else:
         pdfOutput = name
-        
+    if pdfOutput in allFile:
+        print 'Final target name "{}" already exists'.format(pdfOutput)
+        answer = ''
+        while(answer != 'yes' and answer != 'no'):
+            answer = raw_input('Want to proceed and replace this target with the merged file? tap "yes" or "no"\n')
+        if answer == 'no':
+            pdfOutput = 'merged-' + time.strftime('%Y%m%d-%H%M%S') + '.pdf'
+            print 'Ok the target is saved by the name "{}" then'.format(pdfOutput)
+    
     index = 0
     for file in allFile:
-        if '.py' not in file and file != pdfOutput: # 防止重复merge
+        if '.py' not in file: # 防止重复merge
             try:
                 target = open(file, 'rb')
                 mergeFile.append(target)
@@ -28,7 +65,13 @@ def mergeAll(name):
     print 'All done!'
     
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Wrong input, the standard input is python PDFMergeHelper.py targetName'
+    if len(sys.argv) < 3:
+        print 'Wrong input, the standard input is either "python PDFMergeHelper.py pdfname... targetName" or "python PDFMergeHelper.py all targetName"' 
         exit()
-    mergeAll(sys.argv[1])
+    if sys.argv[1] == 'all':
+        mergeAll(sys.argv[2])
+    else:
+        mergeTarget(sys.argv[1:-1], sys.argv[-1])
+        
+        
+        
